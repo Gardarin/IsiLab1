117b573d6c98fc712ce18d10eb6e626c374c52de
Gardarin <gardarinigor@gmail.com>
2015-11-04 00:23:24 +0200

    Lab1

---
+++ IsiLab1/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsiLab1
+{
+    class Item
+    {
+        public string Name;
+        public int Count;
+
+        public Item(string name, int count)
+        {
+            Name = name;
+            Count = count;
+        }
+    }
+}
